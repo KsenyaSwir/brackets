85c04af2e243d45ecb5f2536afcbd9fd3b6e0281
KsenyaSwir <38785982+KsenyaSwir@users.noreply.github.com>
2020-03-09 22:04:27 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+
+  let newConfig = config.map(item => item.join(''));
+  let bracket = "";
+  let result = str;
+
+  for (let j = 0; j < str.length; j++) {
+      for (let i = 0; i < newConfig.length; i++) {
+          if (result.includes(newConfig[i])) {
+              bracket = newConfig[i];
+
+              result = result.replace(bracket, "");
+              console.log(result);
+              i--;
+          }
+      }
+  }
+  if (result === '') {
+      return true;
+  } else {
+      return false;
+  }
 }
